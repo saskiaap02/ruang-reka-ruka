f31742c94a7e8e4f44d58b75daedfd3da054b306
mela-9 <hilmara0102@gmail.com>
2026-04-09 08:16:35 +0700

    update: kode app/model/

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Group extends Model
 {
-    //
+    protected $fillable = ['project_class_id', 'nama_kelompok', 'project_title'];
+
+    public function projectClass(): BelongsTo
+    {
+        return $this->belongsTo(ProjectClass::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function logs(): HasMany
+    {
+        return $this->hasMany(ActivityLog::class);
+    }
 }
