0376c396f4c159e798e32b9f0a5f99f0a07293f6
saskiaap02 <putrisaskia991@gmail.com>
2026-04-16 04:10:59 +0700

    Update create_nudges_table.php

--- database/migrations/2026_04_09_020131_create_nudges_table.php
+++ database/migrations/2026_04_09_020131_create_nudges_table.php
@@ -11,8 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+       
         Schema::create('nudges', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('dosen_id')->constrained('users');
+            $table->foreignId('student_id')->constrained('users');
+            $table->foreignId('group_id')->constrained('groups');
+            $table->string('message');
+            $table->boolean('is_read')->default(false);
             $table->timestamps();
         });
     }
